31073d3af646eae418e270ccd273d7ddd28d4552
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-08-26 01:14:58 +0900

    Add script to build kubernetes-client for fetch API

---
+++ config/settings.sh
@@ -0,0 +1,14 @@
+# GitHub username/organization to clone kubernetes repo from.
+export USERNAME=kubernetes
+
+# Kubernetes branch/tag to get the OpenAPI spec from.
+export KUBERNETES_BRANCH="v1.18.0"
+
+# client version for packaging and releasing. It can
+# be different than SPEC_VERSION.
+export CLIENT_VERSION="0.0.0"
+
+# Name of the release package
+export PACKAGE_NAME="kubernetes-fetch-client"
+
+export OPENAPI_GENERATOR_COMMIT=v4.3.1

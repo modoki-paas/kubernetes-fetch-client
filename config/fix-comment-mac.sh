1a70cb141016543d726df564dd6fa9d82b2113d8
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-08-27 21:41:57 +0900

    Merge fix-comment-mac.sh into fix-comment.sh

--- config/fix-comment-mac.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-find ./src -type f | xargs sed -i '' "s/'\\*\\//'\\* \\//g"
-find ./src -type f | xargs sed -i '' "s/\"\\*\\//\"\\* \\//g"
-
-sed -i '' "s/export \\* from '\\.\\/\\(.*\\)'/export {\\1} from '.\\/\\1'/g" ./src/apis/index.ts
--- config/fix-comment.sh
+++ config/fix-comment.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
 
-find ./src -type f | xargs sed -i "s/'\\*\\//'\\* \\//g"
-find ./src -type f | xargs sed -i "s/\"\\*\\//\"\\* \\//g"
+if [ "$(uname)" == 'Darwin' ]; then
+    # Mac
 
-sed -i "s/export \\* from '\\.\\/\\(.*\\)'/export {\\1} from '.\\/\\1'/g" ./src/apis/index.ts
+    find ./src -type f | xargs sed -i '' "s/'\\*\\//'\\* \\//g"
+    find ./src -type f | xargs sed -i '' "s/\"\\*\\//\"\\* \\//g"
+
+    sed -i '' "s/export \\* from '\\.\\/\\(.*\\)'/export {\\1} from '.\\/\\1'/g" ./src/apis/index.ts
+else
+    # Linux
+
+    find ./src -type f | xargs sed -i "s/'\\*\\//'\\* \\//g"
+    find ./src -type f | xargs sed -i "s/\"\\*\\//\"\\* \\//g"
+
+    sed -i "s/export \\* from '\\.\\/\\(.*\\)'/export {\\1} from '.\\/\\1'/g" ./src/apis/index.ts
+fi
+

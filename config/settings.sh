813ae2418319b13329ebe212f612082a69dc337b
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-09-01 13:35:35 +0900

    Update to 0.0.4

--- config/settings.sh
+++ config/settings.sh
@@ -6,7 +6,7 @@ export KUBERNETES_BRANCH="master"
 
 # client version for packaging and releasing. It can
 # be different than SPEC_VERSION.
-export CLIENT_VERSION="0.0.3"
+export CLIENT_VERSION="0.0.4"
 
 # Name of the release package
 export PACKAGE_NAME="@modoki-paas/kubernetes-fetch-client"
